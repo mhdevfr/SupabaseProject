6b55fcddd999067bb8317f47e7f927c4412d4a85
milan <milan.herbe@gmail.com>
2024-04-24 01:20:05 +0200

    p

--- pages/index.vue
+++ pages/index.vue
@@ -36,4 +36,28 @@
 </style>
 
 <script setup lang="ts">
+import useSupabaseAuthClient from '@nuxtjs/supabase';
+
+const client = useSupabaseAuthClient()
+const user = useSupabaseUser()
+const loading = ref(false)
+
+const logout = async () => {
+  loading.value = true
+  const { error } = await client.auth.signOut()
+  if (error) {
+    loading.value = false
+    return alert('Something went wrong !')
+  }
+}
+
+useHead({
+  title: 'supaAuth',
+  meta: [
+    { name: 'description', content: 'Authentication template with email and password, using Supabase. If you want to a quick start to your next Nuxt3 app, please feel free to use this template.' }
+  ]
+})
+
+
+
 </script>
